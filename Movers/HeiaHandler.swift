399b8cf33896f820196f6345b63413750dc8d3af
Michael Holler <michael.holler@reaktor.fi>
2016-03-14 09:55:06 +0200

    Secrets to ignore

---
+++ Movers/HeiaHandler.swift
@@ -0,0 +1,9 @@
+//
+//  HeiaHandler.swift
+//  Movers
+//
+//  Created by Michael Holler on 14/03/16.
+//  Copyright © 2016 Holler. All rights reserved.
+//
+
+import Foundation
---
+++ Movers/Secrets.swift
@@ -0,0 +1,9 @@
+//
+//  Secrets.swift
+//  Movers
+//
+//  Created by Michael Holler on 14/03/16.
+//  Copyright © 2016 Holler. All rights reserved.
+//
+
+import Foundation
